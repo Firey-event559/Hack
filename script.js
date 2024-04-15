d21ba3b1ecd4a3d1f37d275c3434b480b219bdcf
Vasco van Gils <vascovangils@gmail.com>
2024-04-15 09:32:22 +0200

    Run away button

---
+++ script.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("runaway-button")
+  .addEventListener("click", function () {
+    var button = this;
+    var newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
+    var newPositionY =
+      Math.random() * (window.innerHeight - button.offsetHeight);
+
+    button.style.left = newPositionX + "px";
+    button.style.top = newPositionY + "px";
+  });
